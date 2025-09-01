b88d452b3611374807adaa1aaa2f6fed0db5102e
Nelysistrying <abnelys.coriano@upr.edu>
2025-09-01 13:50:18 -0400

    Update projectData.js

--- src/data/projectData.js
+++ src/data/projectData.js
@@ -3,16 +3,16 @@ export const projects = [
   {
     id: 1,
     title: {
-      en: "",
-      es: "",
+      en: "One Day at a time",
+      es: "Un Día a la Vez",
     },
     description: {
-      en: "",
-      es: "",
+      en: "It's a website with the purpose of creating awareness about mental health and also some tips for living day to day with different mental illnesses.",
+      es: "Es una pagina web dedicada a crear conciencia sobre la salud mental y también algunos consejos para vivir día a día con diferentes enfermedades mentales.",
     },
     image: "",
-    liveSite: "",
-    githubRepo: "",
+    liveSite: "https://nelysistrying.github.io/Un_Dia_a_la_vez/",
+    githubRepo: "https://github.com/Nelysistrying/Un_Dia_a_la_vez",
   },
   // Add more projects here...
 ];
